766837d9b24de18d9309d60f204bdb3d09edd77e
sonamng <sonam20@navgurukul.org>
2022-05-30 10:35:04 +0530

    first commit

---
+++ src/component/Hello.js
@@ -0,0 +1,72 @@
+// if-esle  for condition in functional component :-
+
+
+// import React from "react";
+// function Show(){
+//     const num=123;
+//     if(num===1233){
+//         return(<h1>You can do </h1>)
+//     }else{
+//         return(<h1>You can't do it</h1>)
+
+//     }
+
+// }
+// export default Show;
+
+
+
+
+
+
+// turnary for condition in functional component :-
+
+
+function Sonam(){
+    const name="rajeev";
+    return(
+        <div>
+            {name==="sonam"?
+                <h1>welcome user</h1>:<h1>dont come</h1>
+            }
+        </div>
+    )
+}
+export default Sonam;
+
+
+
+
+
+// switch case for condition in  functional component:-
+
+// function  Render(){
+//     switch("sonam"){
+//         case 1:
+//             return(<h1>yah you are right</h1>)
+//             default:
+//                 return(<h1>you have to do again </h1>)
+//     }
+// }
+// export default Render;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
